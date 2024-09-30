0ef559f355e3a270e9af973cb262f09c4ee6e1c2
adriantilvar <62797798+adriantilvar@users.noreply.github.com>
2024-09-30 14:35:28 +0300

    made the body bg lighter

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,7 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪧</text></svg>"
         />
       </head>
-      <body className="flex h-screen w-screen flex-col bg-zinc-100">
+      <body className="flex h-screen w-screen flex-col bg-zinc-50">
         <main className="flex-1 p-6">{children}</main>
         <footer className="px-6 py-2 text-xs italic text-zinc-400">
           The Lean Canvas, adapted by Ash Maurya from the Business Model Canvas,
