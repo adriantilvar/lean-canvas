70d9fbe789dffeef3f87a6a13272cb4266ddda06
adriantilvar <62797798+adriantilvar@users.noreply.github.com>
2024-09-30 14:34:07 +0300

    added credit for The Lean Canvas

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,8 +20,12 @@ export default function RootLayout({
           href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🪧</text></svg>"
         />
       </head>
-      <body className="h-screen w-screen bg-zinc-100">
-        <main className="h-full p-6">{children}</main>
+      <body className="flex h-screen w-screen flex-col bg-zinc-100">
+        <main className="flex-1 p-6">{children}</main>
+        <footer className="px-6 py-2 text-xs italic text-zinc-400">
+          The Lean Canvas, adapted by Ash Maurya from the Business Model Canvas,
+          CC BY-SA 3.0 license.
+        </footer>
       </body>
     </html>
   );
